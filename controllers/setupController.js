0a1a78a67070046e6c8ec35f826c2ef181486439
henry <henryyogman@gmail.com>
2016-07-24 12:28:11 -0600

    send to remote repo

---
+++ controllers/setupController.js
@@ -0,0 +1,38 @@
+var Todos = require('../models/todoModels');  
+
+module.exports = function(app){
+
+	app.get('/api/setupTodos', function(req, res){
+		// seed database 
+
+		var starterTodos = [
+			{
+				username:'test',
+				todo:'buy milk', 
+				isDone:false,
+				hasAttachment:false
+
+			},
+			{
+				username:'test',
+				todo:'feed dog',
+				isDone: false, 
+				hasAttachment:false
+			},
+			{
+				username:'test',
+				todo:'learn node',
+				isDone:false, 
+				hasAttachment:false 
+			}
+
+		];
+
+		Todos.create(starterTodos, function(err, results){
+			res.send(results);
+		});
+
+	});
+
+
+}
